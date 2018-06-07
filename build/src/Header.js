eb702c42b9b7f96c7ffe50e15821afac9b920608
blutorange <wachsmuth.andre@gmx.de>
2018-06-07 16:45:59 +0200

    remove rogue build files

--- build/src/Header.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var IncrementalTopologicalSortAlgorithm;
-(function (IncrementalTopologicalSortAlgorithm) {
-    IncrementalTopologicalSortAlgorithm[IncrementalTopologicalSortAlgorithm["PK"] = 0] = "PK";
-})(IncrementalTopologicalSortAlgorithm = exports.IncrementalTopologicalSortAlgorithm || (exports.IncrementalTopologicalSortAlgorithm = {}));
--- build/src/PkHeader.d.ts
+++
@@ -1,9 +0,0 @@
-import { ArrayFrom, VertexData } from "./Header";
-export interface PkVertexData extends VertexData {
-    order: number;
-    visited: boolean;
-}
-export interface PkOptions {
-    Set?: SetConstructor;
-    ArrayFrom?: ArrayFrom;
-}
